1710a57c9d5e815e27f21dc924d5e402991f898d
Guilherme Constantino <guilherme.constantino.1b@gmail.com>
2025-09-09 14:18:37 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const colors = require('colors');
+
+console.log('📦 Bem-vindo à Padaria do João!'.rainbow)
+console.log('Pães frescos todos os dias!'.green);
+console.log('Horário: 6h às 18h'.blue);
